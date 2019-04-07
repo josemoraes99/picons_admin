1065d568f4ce4b6e2fc719a4a94ea8928f1146d3
Jose Moraes <josemoraes@mbpretina.lan>
2019-04-07 20:11:12 -0300

    [update_07_09_2019]

---
+++ docs/script.js
@@ -0,0 +1,14 @@
+/*jshint esversion: 6 */
+"use strict";
+
+let cnt1=[];
+let optSelec='';
+//$('.slcI').append(optSelec);
+let menuC = 'menuCshow';
+function myAlertTop(){
+    $(".myAlert-top").show();
+    setTimeout(function(){
+        $(".myAlert-top").hide(); 
+    }, 2000);
+}
+console.log("ok");
