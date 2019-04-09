0c7a0517319cf47f403716832626332f8446797d
Jose Moraes <josemoraes@mbpretina.lan>
2019-04-09 17:12:26 -0300

    [update_09_04_2019]

--- docs/script.js
+++ docs/script.js
@@ -1,7 +1,7 @@
 /*jshint esversion: 6 */
 "use strict";
 
-// version 0.3
+// version 0.4
 
 let channelList;
 let __authkey__ = false;
@@ -143,12 +143,13 @@ function readCookie(name) {
 }
 
 function deleteAllCookies() {
-    var cookies = document.cookie.split(";");
+    let cookies
+    cookies = document.cookie.split(';');
 
-    for (var i = 0; i < cookies.length; i++) {
-        var cookie = cookies[i];
-        var eqPos = cookie.indexOf("=");
-        var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+    for (let i = 0; i < cookies.length; i++) {
+        let cookie = cookies[i];
+        let eqPos = cookie.indexOf("=");
+        let name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
         document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
     }
 }
