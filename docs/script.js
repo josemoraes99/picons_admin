dcd9642c3b1d14fe8bb6382b3bfdbf03d1d70b20
Jose Moraes <josemoraes@mbpretina.lan>
2019-05-27 14:24:17 -0300

    [update_27_05_2019]

--- docs/script.js
+++ docs/script.js
@@ -6,17 +6,17 @@
 let channelList;
 let __authkey__ = false;
 
-function myAlertTop(msg){
+function myAlertTop(msg) {
     document.getElementById("alertMsg").innerHTML = msg;
     $(".myAlert-top").show();
-    setTimeout(function(){
-        $(".myAlert-top").hide(); 
+    setTimeout(function () {
+        $(".myAlert-top").hide();
     }, 2000);
 }
 
 const getListChannels = async () => {
     console.log("load list");
-    if ( __authkey__ ){
+    if (__authkey__) {
         let response = await fetch('https://1wdtecach7.execute-api.sa-east-1.amazonaws.com/prod/list/' + __authkey__);
         let data = await response.json();
         let resp = JSON.parse(data);
@@ -25,13 +25,13 @@ const getListChannels = async () => {
     }
 };
 
-$("#menuPiconShow").click(function(){
+$("#menuPiconShow").click(function () {
     // console.log("painelNew");
     $(".painelPicons").hide();
     $(".painelNew").show();
 });
 
-$("#menuListaShow").click(function(){
+$("#menuListaShow").click(function () {
     // console.log("painelLista");
     $(".painelPicons").hide();
     $(".painelLista").show();
@@ -39,24 +39,25 @@ $("#menuListaShow").click(function(){
 
 const refreshChannels = async () => {
     console.log("refresh list");
-    if ( __authkey__ ){
+    if (__authkey__) {
         let response = await fetch('https://1wdtecach7.execute-api.sa-east-1.amazonaws.com/prod/refresh/' + __authkey__);
-        let data = await response.json();
-        let resp = JSON.parse(data);
+        let resp = await response.json();
+        // let data = await response.json();
+        // let resp = JSON.parse(data);
         checkAuth(resp.authenticated);
-        if ( resp.refreshed == "true" ){
+        if (resp.refreshed == "true") {
             myAlertTop("Atualizado!");
             await funcGetChannelsList();
         }
     }
 };
 
-$("#menuListaRefresh").click(function(){
+$("#menuListaRefresh").click(function () {
     // console.log("refresh");
     refreshChannels();
 });
 
-$("#menuLogout").click(function(){
+$("#menuLogout").click(function () {
     __authkey__ = false;
     writeCookie('sessionId', __authkey__, -1);
     showPage();
@@ -64,11 +65,11 @@ $("#menuLogout").click(function(){
 
 function dynamicSort(property) {
     let sortOrder = 1;
-    if(property[0] === "-") {
+    if (property[0] === "-") {
         sortOrder = -1;
         property = property.substr(1);
     }
-    return function (a,b) {
+    return function (a, b) {
         let result = (a[property] < b[property]) ? -1 : (a[property] > b[property]) ? 1 : 0;
         return result * sortOrder;
     };
@@ -76,7 +77,7 @@ function dynamicSort(property) {
 
 const changeChannelStat = async (pic, redir) => {
     console.log("change channel", pic, redir);
-    if ( __authkey__ ){
+    if (__authkey__) {
         // alterar api
         let response = await fetch('https://1wdtecach7.execute-api.sa-east-1.amazonaws.com/prod/change/' + pic + '/' + redir + '/' + __authkey__);
         let data = await response.json();
@@ -84,15 +85,15 @@ const changeChannelStat = async (pic, redir) => {
         checkAuth(resp.authenticated);
         // console.log(resp);
         // console.log(resp.changed);
-        if ( resp.changed == "true" ){
+        if (resp.changed == "true") {
             // console.log("oook");
             myAlertTop("Alterado!");
         }
         // alterar api
 
-        channelList.forEach(function (itemChannel, index){
-            if ( itemChannel.channell == pic ){
-                if ( redir != 1 ) {
+        channelList.forEach(function (itemChannel, index) {
+            if (itemChannel.channell == pic) {
+                if (redir != 1) {
                     itemChannel.stat = "redir";
                     itemChannel.redir = redir;
                 } else {
@@ -108,7 +109,7 @@ const changeChannelStat = async (pic, redir) => {
 const removeChannel = async (chn) => {
     console.log("change channel", chn);
 
-    if ( __authkey__ ){
+    if (__authkey__) {
         // remover api
         let response = await fetch('https://1wdtecach7.execute-api.sa-east-1.amazonaws.com/prod/remove/' + chn + '/' + __authkey__);
         let data = await response.json();
@@ -117,7 +118,7 @@ const removeChannel = async (chn) => {
         checkAuth(resp.authenticated);
         // console.log(resp);
         // console.log(resp.changed);
-        if ( resp.removed == "true" ){
+        if (resp.removed == "true") {
             // console.log("oook");
             myAlertTop("Removido!");
         }
@@ -125,17 +126,17 @@ const removeChannel = async (chn) => {
 
         for (let i = 0; i < channelList.length; i++) {
             const nome = channelList[i].channell;
-            if ( nome == chn ){
-                channelList.splice(i,1);
+            if (nome == chn) {
+                channelList.splice(i, 1);
             }
-         }
+        }
         processChannelList(channelList);
     }
 };
 
 const changeChannelCategorie = async (pic, newCat) => {
     console.log("change channel", pic, newCat);
-    if ( __authkey__ ){
+    if (__authkey__) {
         // alterar api
         let response = await fetch('https://1wdtecach7.execute-api.sa-east-1.amazonaws.com/prod/changecategorie/' + pic + '/' + newCat + '/' + __authkey__);
         let data = await response.json();
@@ -143,10 +144,10 @@ const changeChannelCategorie = async (pic, newCat) => {
         checkAuth(resp.authenticated);
         // console.log(resp);
         // console.log(resp.changed);
-        if ( resp.changed == "true" ){
+        if (resp.changed == "true") {
             // console.log("oook");
             myAlertTop("Alterado!");
-            await funcGetChannelsList();
+            await funcGetChannelsList()
         }
         // alterar api
 
@@ -165,11 +166,11 @@ const changeChannelCategorie = async (pic, newCat) => {
     }
 };
 
-function writeCookie(name,value,days) {
+function writeCookie(name, value, days) {
     let date, expires;
     if (days) {
         date = new Date();
-        date.setTime(date.getTime()+(days*24*60*60*1000));
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
         expires = "; expires=" + date.toGMTString();
     } else {
         expires = "";
@@ -180,40 +181,40 @@ function writeCookie(name,value,days) {
 function readCookie(name) {
     let i, c, ca, nameEQ = name + "=";
     ca = document.cookie.split(';');
-    for(i=0;i < ca.length;i++) {
+    for (i = 0; i < ca.length; i++) {
         c = ca[i];
-        while (c.charAt(0)==' ') {
-            c = c.substring(1,c.length);
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1, c.length);
         }
         if (c.indexOf(nameEQ) == 0) {
-            return c.substring(nameEQ.length,c.length);
+            return c.substring(nameEQ.length, c.length);
         }
     }
     return '';
 }
 
-function afterDomChange(){
-    $('.selectChannelWithFile').change(function() {
-        const id  = $(this).attr("id").replace("sele.", "");
+function afterDomChange() {
+    $('.selectChannelWithFile').change(function () {
+        const id = $(this).attr("id").replace("sele.", "");
         const newval = $(this).val();
         changeChannelStat(id, newval);
     });
 
-    $('.selectCategorie').change(function() {
-        const id  = $(this).attr("id").replace("seleCat.", "");
+    $('.selectCategorie').change(function () {
+        const id = $(this).attr("id").replace("seleCat.", "");
         const newval = $(this).val();
         changeChannelCategorie(id, newval);
     });
 
-    $('.btnRemove').click(function() {
-        const id  = $(this).attr("id").replace("button.", "");
+    $('.btnRemove').click(function () {
+        const id = $(this).attr("id").replace("button.", "");
         console.log(id);
         // const newval = $(this).val();
         removeChannel(id);
     });
 }
 
-function processChannelList(chanArr){
+function processChannelList(chanArr) {
     const categorias = ['variedades', 'interno', 'adultos'];
     let divPainelNew = document.getElementsByClassName('painelNew')[0];
     let divPainelLista = document.getElementsByClassName('painelLista')[0];
@@ -240,7 +241,7 @@ function processChannelList(chanArr){
     }
 
     chanArr.forEach(function (itemChannel, index) {
-        if ( itemChannel.stat == "file" ){
+        if (itemChannel.stat == "file") {
             let opt1 = document.createElement("option");
             opt1.text = itemChannel.channell;
             sel.add(opt1, null);
@@ -257,8 +258,8 @@ function processChannelList(chanArr){
         divCol2.className = "col-1";
         divCol3.className = "col-2";
         let label1Txt = document.createTextNode(itemChannel.channell);
-        
-        if ( itemChannel.stat == "undef" ){
+
+        if (itemChannel.stat == "undef") {
             divCol1.appendChild(label1Txt);
 
             let newSelect = sel.cloneNode(true);
@@ -273,7 +274,9 @@ function processChannelList(chanArr){
             removeBtn.innerHTML = "Remover";
             divCol3.appendChild(removeBtn);
 
-{/* <button type="button" class="btn btn-sm btn-danger" id="btnR%s">Remover</button> */}
+            {
+                /* <button type="button" class="btn btn-sm btn-danger" id="btnR%s">Remover</button> */
+            }
 
             divRow.appendChild(divCol1);
             divRow.appendChild(divCol2);
@@ -287,7 +290,7 @@ function processChannelList(chanArr){
             divCol3.appendChild(newSelectCat);
             newSelectCat.value = itemChannel.categoria;
 
-            if ( itemChannel.stat != "file" ){
+            if (itemChannel.stat != "file") {
                 let newSelect = sel.cloneNode(true);
                 newSelect.className = "selectChannelWithFile";
                 newSelect.id = "sele." + itemChannel.channell;
@@ -313,8 +316,8 @@ const funcGetChannelsList = async () => {
     processChannelList(channelList);
 };
 
-function checkAuth(au){
-    if ( au === false ){
+function checkAuth(au) {
+    if (au === false) {
         __authkey__ = false;
         showPage();
     } else {
@@ -322,35 +325,35 @@ function checkAuth(au){
     }
 }
 
-$(".loginBtn").click(function(){
+$(".loginBtn").click(function () {
     const usr = document.getElementById("inputUsername").value.trim();
     const pwd = document.getElementById("inputPassword").value.trim();
     processLogin(usr, pwd);
-    document.getElementById("inputUsername").value ='';
-    document.getElementById("inputPassword").value ='';
+    document.getElementById("inputUsername").value = '';
+    document.getElementById("inputPassword").value = '';
 });
 
 const processLogin = async (usr, pwd) => {
     console.log("login", usr, pwd);
-    let response = await fetch('https://1wdtecach7.execute-api.sa-east-1.amazonaws.com/prod/login/' + usr + "/" +pwd);
+    let response = await fetch('https://1wdtecach7.execute-api.sa-east-1.amazonaws.com/prod/login/' + usr + "/" + pwd);
     let data = await response.json();
     let resp = JSON.parse(data);
-    if ( resp.authenticated ){
+    if (resp.authenticated) {
         // console.log("ok");
         __authkey__ = resp.authcode;
         writeCookie('sessionId', __authkey__, 3);
         showPage();
     } else {
         $(".loginerrormsg").show();
-         setTimeout(function(){
-            $(".loginerrormsg").hide(); 
+        setTimeout(function () {
+            $(".loginerrormsg").hide();
         }, 5000);
     }
     // console.log(resp);
 };
 
 const showPage = async () => {
-    if ( __authkey__ ){
+    if (__authkey__) {
         $("#painelLogin").hide();
         $(".painelNew").show();
         $("#painelPrincipal").show();
@@ -370,7 +373,7 @@ const main = async () => {
 };
 
 let id_cookie = readCookie('sessionId');
-if (id_cookie != "" ){
+if (id_cookie != "") {
     __authkey__ = id_cookie;
 }
 
